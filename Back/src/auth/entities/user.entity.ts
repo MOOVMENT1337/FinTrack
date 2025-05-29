cd488032d8a8e530d24417a7e221ea65c796a385
drkl1ght <kir.safronov@gmail.com>
2025-05-29 10:19:42 +0300

    Убрал лишнюю папку от JS бэка. Добавил роль админа..
    Роль администратора выдается через БД и при регистрации не уточняется.

--- Back/src/auth/entities/user.entity.ts
+++ Back/src/auth/entities/user.entity.ts
@@ -2,6 +2,11 @@ import { Entity, PrimaryGeneratedColumn, Column, CreateDateColumn, BeforeInsert,
 import { Operation } from '../../finance/entities/operation.entity';
 import * as bcrypt from 'bcryptjs';
 
+export enum UserRole {
+  USER = 'user',
+  ADMIN = 'admin',
+}
+
 @Entity('users')
 export class User {
   @PrimaryGeneratedColumn()
@@ -19,6 +24,13 @@ export class User {
   @CreateDateColumn()
   createdAt: Date = new Date(); // Автоматически инициализируется TypeORM
 
+  @Column({
+    type: 'enum',
+    enum: UserRole,
+    default: UserRole.USER,
+  })
+  role: UserRole = UserRole.USER;
+
   @BeforeInsert()
   async hashPassword() {
     this.password = await bcrypt.hash(this.password, 10);
@@ -29,5 +41,9 @@ export class User {
 
   async comparePassword(attempt: string): Promise<boolean> {
     return await bcrypt.compare(attempt, this.password);
+  }
+
+  isAdmin(): boolean {
+    return this.role === UserRole.ADMIN;
   }
 }
